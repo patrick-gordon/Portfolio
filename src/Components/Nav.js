2ae6c557749a338b11747a4c62ad15cd449cf3bb
Patrick Gordon <patrickgordon42@yahoo.com>
2019-10-10 21:50:07 -0500

    nav bar styling

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-import {Link} from 'react-router-dom';
+const Navbar = styled.div`
+height: 30px;
+text-decoration: none;
+display: flex;
+justify-content: space-evenly;
+background-color: #071a91;
+color: #ab5f0e;
+`;
 
 
 export default function Nav() {
     return (
-        <div className='nav'>
+        <Navbar>
             <Link to='/'>Home</Link>
             <Link to='/about'>About</Link>
             <Link to='/contact'>Contact</Link>
-            
-        </div>
+        </Navbar>
     )
 }
