c4e2fd0fc841fe4d5d537f3eeca4a4c86b5d26ec
patrick-gordon <patrickgordon42@yahoo.com>
2019-12-18 16:25:06 -0600

    made .map to map over repos and make a vard for each repo

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -5,77 +5,65 @@ import Axios from 'axios';
 
 const Style = styled.div`
     display: flex;
-    // flex-direction: column;
+    
     align-items: baseline;
-    // margin-top: 10rem;
+    
 `;
 
 export default function Projects() {
     const [error, setError] = useState('');
     const [load, setLoad] = useState(false);
-    const [project, setProject] = useState([])
+    const [projects, setProjects] = useState([])
 
     useEffect(() => {
         Axios
             .get('https://api.github.com/users/patrick-gordon/repos')
             .then(res=> {
                 console.log(res.data)
-                // setProject(res.data)
+                setProjects(res.data)
                 setLoad(true)
             })
             .catch(err => {
                 setError(err.message);
                 setLoad(false)
             })
-    })
-
-    if(project.name === 'MERN-blog' || project.name === 'React-Instagram-App' || project.name === 'impact-dashboard') {
-        return(
-        <div>
-            <Container>
-            <Style>
-                <Card style={{ width: '18rem', margin:'2rem', height: '20rem', backgroundColor: '#7a7573' }}>
-                <Card.Img variant="top" src="holder.js/100px180"></Card.Img>
-                <Card.Body>
-                    <Card.Title>Project 1</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button href='https://github.com/patrick-gordon/MERN-blog' style={{ backgroundColor: '#ab694f', border: '1px solid white' }} variant="primary">Github repo</Button>
-                </Card.Body>
-                </Card>
-
-                <Card style={{ width: '18rem', margin:'2rem',  height: '20rem', backgroundColor: '#7a7573' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Project 2</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button style={{margin:'60px', backgroundColor: '#ab694f', border: '1px solid white'}} variant="primary">Go somewhere</Button>
-                </Card.Body>
-                </Card>
-
-                <Card style={{ width: '18rem', margin:'3rem',  height: '20rem', backgroundColor: '#7a7573' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                    <Card.Title>Project 3</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button style={{margin:'60px', backgroundColor: '#ab694f', border: '1px solid white'}} variant="primary">Go somewhere</Button>
-                </Card.Body>
-                </Card>
-            </Style>
-            </Container>
-        </div>
-    )
-} else {
-    return(
-        <div>Can not get Project</div>
-        )
+        },[])
+        
+        
+        
+        return (
+            <>
+            {/* <h1> HI {console.log('[checking projects]', projects)} </h1> */}
+            {projects.map(repo => {
+                if (repo.name.includes('java')) {
+                    return (
+                        <div>
+                            <Container>
+                                <Style>
+                                    <Card style={{ width: '18rem', margin:'2rem', height: '20rem', backgroundColor: '#7a7573' }}>
+                                    <Card.Img variant="top" src="holder.js/100px180"></Card.Img>
+                                    <Card.Body>
+                                        <Card.Title>{repo.name}</Card.Title>
+                                        <Card.Text>
+                                        Some quick example text to build on the card title and make up the bulk of
+                                        the card's content.
+                                        </Card.Text>
+                                        <Button href={`http://www.github.com/patrick-gordon/${repo.name}`} style={{ backgroundColor: '#ab694f', border: '1px solid white' }} variant="primary">Github repo</Button>
+                                    </Card.Body>
+                                    </Card>
+                                </Style>
+                            </Container>
+                        </div>  
+                    );
+            } else {
+                console.log('MERN not found');
+            }
+            })}
+            </>  
+        );
     }
-}
+
+
+
+
+
